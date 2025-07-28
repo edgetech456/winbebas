e979adb99e21686ec57ecae30b4700bb712bc97f
weiyee <edgetechnology456@gmail.com>
2025-07-28 17:25:22 +0800

    update apk link

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
     if (/android/i.test(userAgent)) {
-        downloadBtn.href = "https://images.6440949940.com/wsd-images-prod/win4umyrf6/merchant_resource/android/win4umyrf6_2.4.67_20250623165028.apk";
+        downloadBtn.href = "https://images.6440949940.com/wsd-images-prod/winbemyrf6/merchant_resource/android/winbemyrf6_2.4.68_20250726120129.apk";
         deviceIcon.src = "images/icon-android.png";
     } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        downloadBtn.href = "https://images.6440949940.com/wsd-images-prod/win4umyrf6/merchant_resource/mobileconfig/win4umyrf6_2.4.3_20250623165010.mobileconfig";
+        downloadBtn.href = "https://images.6440949940.com/wsd-images-prod/winbemyrf6/merchant_resource/mobileconfig/winbemyrf6_2.4.3_20250726120113.mobileconfig";
         deviceIcon.src = "images/icon-ios.png";
     } else {
-        downloadBtn.href = "https://images.6440949940.com/wsd-images-prod/win4umyrf6/merchant_resource/android/win4umyrf6_2.4.67_20250623165028.apk";
+        downloadBtn.href = "https://images.6440949940.com/wsd-images-prod/winbemyrf6/merchant_resource/android/winbemyrf6_2.4.68_20250726120129.apk";
         deviceIcon.src = "images/icon-android.png";
     }
 });
